c8da8e309c2c192f70a7399d4313acf94c185626
PatricioOsorio <patriciomiguel_12@hotmail.com>
2023-06-15 14:52:47 -0600

    Prueba cards

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,25 +6,43 @@ const products = [
     id: 1,
     title: 'Product 1',
     description: 'lorem',
-    src: 'https://www.jquery-az.com/html/images/banana.jpg',
+    src: 'https://cubomuebles.com.mx/412-large_default/silla-infantil-reforzadas-cu-sk02b.jpg',
   },
   {
     id: 2,
     title: 'Product 2',
     description: 'lorem',
-    src: 'https://www.jquery-az.com/html/images/banana.jpg',
+    src: 'https://cubomuebles.com.mx/412-large_default/silla-infantil-reforzadas-cu-sk02b.jpg',
   },
   {
     id: 3,
     title: 'Product 3',
     description: 'lorem',
-    src: 'https://www.jquery-az.com/html/images/banana.jpg',
+    src: 'https://cubomuebles.com.mx/412-large_default/silla-infantil-reforzadas-cu-sk02b.jpg',
   },
   {
     id: 4,
     title: 'Product 4',
     description: 'lorem',
-    src: 'https://www.jquery-az.com/html/images/banana.jpg',
+    src: 'https://cubomuebles.com.mx/412-large_default/silla-infantil-reforzadas-cu-sk02b.jpg',
+  },
+  {
+    id: 5,
+    title: 'Product 4',
+    description: 'lorem',
+    src: 'https://cubomuebles.com.mx/412-large_default/silla-infantil-reforzadas-cu-sk02b.jpg',
+  },
+  {
+    id: 6,
+    title: 'Product 4',
+    description: 'lorem',
+    src: 'https://cubomuebles.com.mx/412-large_default/silla-infantil-reforzadas-cu-sk02b.jpg',
+  },
+  {
+    id: 7,
+    title: 'Product 4',
+    description: 'lorem',
+    src: 'https://cubomuebles.com.mx/412-large_default/silla-infantil-reforzadas-cu-sk02b.jpg',
   },
 ];
 
@@ -35,7 +53,7 @@ const HomePage = () => {
         <div className="row bg-light shadow rounded mb-5">
           <Hero />
         </div>
-        <div className="row row-cols-1 row-cols-md-3 g-3 ">
+        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 ">
           {products.map((product) => (
             <CardProduct
               key={product.id}
