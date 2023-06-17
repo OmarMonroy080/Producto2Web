6f0035af1e4a4e759dc499882a18f497dfc361fc
Omar <omarpais080@gmail.com>
2023-06-17 15:29:22 -0600

    NosotrosModificacion

--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -1,10 +1,28 @@
 const AboutUsPage = () => {
   return (
     <>
-      <h1>Nosotros</h1>
-      <p>Contenido...</p>
-    </>
-  );
-};
+      <div className="container">
+        <div className="col col-sm-0 col-lg-6"></div>
+        <div className="col col-lg-9 col-sm-12 ">
+        <div class="py-5 text-center">
+          <h1 class="display-4">Nosotros</h1>
+          <p class="lead">Somos una mueblería dedicada a brindarle los mejores muebles para su hogar. En Muebles, entendemos la importancia de tener muebles de calidad que sean funcionales y estéticamente agradables.</p>
 
+          <h2 class="mt-5">Calidad y Durabilidad</h2>
+          <p>Nuestros muebles están fabricados con los mejores materiales y por artesanos expertos en el campo. Cada pieza es cuidadosamente diseñada y construida para garantizar la durabilidad y resistencia.</p>
+
+          <h2 class="mt-5">Amplia Variedad de Estilos</h2>
+          <p>En muebleria, comprendemos que cad a persona tiene su propio estilo y preferencias. Es por eso que ofrecemos una amplia variedad de estilos de muebles, desde modernos y minimalistas hasta clásicos y rústicos. Seguro encontrará algo que se adapte a su gusto.</p>
+
+          <h2 class="mt-5">Atención Personalizada</h2>
+          <p>Nuestro equipo de ventas altamente capacitado está listo para ayudarlo a encontrar los muebles perfectos para su hogar. Brindamos atención personalizada y asesoramiento experto para asegurarnos de que esté satisfecho con su elección.</p>
+
+          <h2 class="mt-5">Visítenos hoy</h2>
+          <p>Estamos ubicados en el centro de la ciudad, en la calle principal. Lo invitamos a visitar nuestra mueblería y explorar nuestra increíble selección de muebles. ¡Esperamos poder ayudarlo a transformar su hogar en un lugar elegante y acogedor!</p>
+        </div>
+      </div>
+      </div>
+    </>
+  )
+};
 export default AboutUsPage;
