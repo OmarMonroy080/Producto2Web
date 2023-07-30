038607bcd2102153d70da1ad18def5410048995e
Omar <omarpais080@gmail.com>
2023-07-30 15:09:01 -0600

    validar Login- crea Rutas admin

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
 import HeaderPublic from './components/HeaderPublic';
 import './App.css';
 import CataloguePage from './pages/CataloguePage';
@@ -19,39 +18,43 @@ import CreateProduct from './pages/CreateProduct';
 import CreateInputOutput from './pages/CreateInputOutput';
 import CreateUser from './pages/CreateUser';
 import Page404 from './pages/Page404';
+import { useState } from 'react';
 
 function App() {
+  const [Autenticado, setAutenticado] = useState({ isLoggedIn: 0, nombre: "" });
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HeaderPublic />}>
-          <Route index element={<HomePage />} />
-          <Route path="login" element={<LoginPage />} />
-          <Route path="register" element={<RegisterPage />} />
-          <Route path="catalogue" element={<CataloguePage />} />
-          <Route path="aboutus" element={<AboutUsPage />} />
-          <Route path="contact" element={<ContactPage />} />
-        </Route>
-        
-        <Route path="/manager" element={<HeaderAdmin />}>
-          <Route index element={<HomeAdminPage />} />
+        {Autenticado.isLoggedIn ===0 ? (
+          <Route path="/" element={<HeaderPublic />}>
+            <Route index element={<HomePage />} />
+            <Route path="login" element={<LoginPage setAutenticado={setAutenticado}/>} />
+            <Route path="catalogue" element={<CataloguePage />} />
+            <Route path="aboutus" element={<AboutUsPage />} />
+            <Route path="contact" element={<ContactPage />} />
+          </Route>
+        ) : (
+          <Route path="/manager" element={<HeaderAdmin setAutenticado ={setAutenticado} />}>
+            <Route index element={<HomeAdminPage nombre= {Autenticado.nombre} />} />
 
-          <Route path="categories" element={<CategoriesPage />}></Route>
-          <Route path="categories/create" element={<CreateCategory />} />
+            <Route path="categories" element={<CategoriesPage />}></Route>
+            <Route path="categories/create" element={<CreateCategory />} />
 
-          <Route path="products" element={<ProductsPage />} />
-          <Route path="products/create" element={<CreateProduct />} />
+            <Route path="products" element={<ProductsPage />} />
+            <Route path="products/create" element={<CreateProduct />} />
 
-          <Route path="outputs-inputs" element={<OutputsInputsPage />} />
-          <Route path="outputs-inputs/create" element={<CreateInputOutput />} />
+            <Route path="outputs-inputs" element={<OutputsInputsPage />} />
+            <Route path="outputs-inputs/create" element={<CreateInputOutput />} />
 
-          <Route path="users" element={<UsersPage />} />
-          <Route path="users/create" element={<CreateUser />} />
+            <Route path="users" element={<UsersPage />} />
+            <Route path="users/create" element={<CreateUser />} />
 
 
-          <Route path="dashboard" element={<DashboardPage />} />
-        </Route>
-          <Route path="*" element={<Page404 />} />
+            <Route path="dashboard" element={<DashboardPage />} />
+          </Route>
+        )
+        }
+        <Route path="*" element={<Page404 />} />
       </Routes>
     </BrowserRouter>
   );
