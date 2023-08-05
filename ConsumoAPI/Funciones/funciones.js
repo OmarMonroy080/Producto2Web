2f9a1cad1a58012ad476014bc22dc538dd1b5d51
Omar <omarpais080@gmail.com>
2023-08-04 21:10:22 -0600

    este es xd

--- ConsumoAPI/Funciones/funciones.js
+++ ConsumoAPI/Funciones/funciones.js
@@ -15,3 +15,14 @@ function onfocus(foco) {
     document.getElementById(foco).focus();
   }
 }
+
+export function esLink(cadena) {
+  // Expresión regular para validar un enlace
+  const enlaceRegex = /^(ftp|http|https):\/\/[^ "]+$/;
+
+  // Expresión regular para validar una imagen con o sin parámetros de consulta
+  const imagenRegex = /\.(jpg|jpeg|png|gif|bmp|webp)(\?.*)?$/i;
+
+  // Comprobamos si la cadena cumple con ambos patrones
+  return enlaceRegex.test(cadena) && imagenRegex.test(cadena);
+}
