61889b439b54e2eecb184db322545a8348aeb0e2
Javier Villalba <villalba.am@hotmail.com>
2023-08-06 00:57:03 -0600

    ventas

--- ConsumoAPI/Ventas/VentasAPI.jsx
+++ ConsumoAPI/Ventas/VentasAPI.jsx
@@ -1,7 +1,4 @@
-
 import { showAlerta } from "../Funciones/funciones";
-
-
 
 export async function ListarVentas() {
   const requestOptions = {
@@ -26,29 +23,76 @@ export async function ListarVentas() {
     // Capturamos cualquier error en la solicitud o en el procesamiento JSON
     showAlerta("Error en la solicitud", "error");
     console.log(error);
+    throw error; // Re-lanzamos el error para que el componente que llama a esta función pueda manejarlo
   }
-
 }
 
+export async function ActualizarVenta(parametros, url) {
+  const requestOptions = {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(parametros),  
+  };
 
-export async function ActualizarGuardarV(metodo, parametros, url) {
-    const requestOptions = {
-      method: metodo,
+  try {
+    const response = await fetch(url, requestOptions);
+    const data = await response.json();
+    const mensaje = data.mensaje;
+    if (mensaje === "ok") {
+      showAlerta(mensaje, "success");
+    }
+  } catch (error) {
+    showAlerta("Error en la solicitud", "error");
+    console.log(error);
+    throw error; // Re-lanzamos el error para que el componente que llama a esta función pueda manejarlo
+  }
+}
+
+export async function EliminarVenta(idVenta) {
+  const requestOptions = {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  try {
+    const response = await fetch(`http://www.muebleriatroncoso.somee.com/api/Ventas/Eliminar/${idVenta}`, requestOptions);
+    const data = await response.json();
+    const mensaje = data.mensaje;
+    if (mensaje === "ok") {
+      showAlerta(mensaje, "success");
+    }
+  } catch (error) {
+    showAlerta("Error en la solicitud", "error");
+    console.log(error);
+    throw error; // Re-lanzamos el error para que el componente que llama a esta función pueda manejarlo
+  }
+}
+const guardarVenta = async (idMueble, idUsuario) => {
+  const parametros = {
+    idMueble: idMueble,
+    idUsuario: idUsuario,
+  };
+
+  try {
+    const response = await fetch("http://www.muebleriatroncoso.somee.com/api/Ventas/Guardar", {
+      method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(parametros),  
-    };
-  
-    try {
-      const response = await fetch(url, requestOptions);
-      const data = await response.json();
-      const mensaje = data.mensaje;
-      if ( mensaje =="ok") {
-        showAlerta(mensaje, "success");
-      }
-    } catch (error) {
-      showAlerta("Error en la solicitud", "error");
-      console.log(error);
+      body: JSON.stringify(parametros),
+    });
+
+    const data = await response.json();
+    const mensaje = data.mensaje;
+    if (mensaje === "ok") {
+      showAlerta("Venta guardada exitosamente", "success");
     }
-  };
+  } catch (error) {
+    showAlerta("Error al guardar la venta", "error");
+    console.log(error);
+  }
+};
--- src/pages/OutputsInputsPage.jsx
+++ src/pages/OutputsInputsPage.jsx
@@ -1,13 +1,40 @@
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { ListarVentas, EliminarVenta } from '../../ConsumoAPI/Ventas/VentasAPI.jsx'; // Importamos EliminarVenta
 
 const OutputsInputsPage = () => {
+  const [ventas, setVentas] = useState([]);
+
+  useEffect(() => {
+    // Llamamos a la función ListarVentas para obtener los datos de la API
+    ListarVentas()
+      .then((data) => {
+        // Actualizamos el estado con los datos de ventas recibidos
+        setVentas(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const handleEliminarVenta = async (idVenta) => {
+    try {
+      await EliminarVenta(idVenta);
+      // Si la eliminación es exitosa, actualizamos el estado de ventas para reflejar el cambio
+      setVentas((prevVentas) => prevVentas.filter((venta) => venta.idVenta !== idVenta));
+    } catch (error) {
+      // Manejar el error si es necesario
+      console.log(error);
+    }
+  };
+
   return (
     <>
-      <h1 className="mb-3">Entradas y salidas</h1>
+      <h1 className="mb-3">Ventas</h1>
 
-      <Link className="btn btn-primary mb-3" to="create">
-        <i className="fas fa-plus-circle"></i> Crear entrada y salida
-      </Link>
+
+      {/* modal */}
+      
 
       <div className="table-responsive shadow">
         <table
@@ -17,30 +44,33 @@ const OutputsInputsPage = () => {
           <thead>
             <tr>
               <th>Id</th>
-              <th>Codigo producto</th>
-              <th>Fecha salida</th>
-              <th>Fecha entrada</th>
+              <th>Nombre del mueble</th>
+              <th>Nombre del usuario</th>
               <th>Acciones</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>data...</td>
-              <td>data...</td>
-              <td>data...</td>
-              <td>data...</td>
-              <td className="d-flex justify-content-center g-3">
-                <a className="btn btn-info mx-1 disabled">
-                  <i className="fas fa-info-circle"></i> data...
-                </a>
-                <a className="btn btn-warning mx-1">
-                  <i className="fas fa-edit"></i> data...
-                </a>
-                <a className="btn btn-danger mx-1">
-                  <i className="fas fa-minus-circle"></i> data...
-                </a>
-              </td>
-            </tr>
+            {ventas.map((venta) => (
+              <tr key={venta.idVenta}>
+                <td>{venta.idVenta}</td>
+                <td>{venta.nombreMueble}</td>
+                <td>{venta.nombreUsuario}</td>
+                <td className="d-flex justify-content-center g-3">
+                  <Link to={`/venta/${venta.idVenta}`} className="btn btn-info mx-1">
+                    <i className="fas fa-info-circle"></i> Detalles
+                  </Link>
+                  <Link to={`/venta/editar/${venta.idVenta}`} className="btn btn-warning mx-1">
+                    <i className="fas fa-edit"></i> Editar
+                  </Link>
+                  <button
+                    className="btn btn-danger mx-1"
+                    onClick={() => handleEliminarVenta(venta.idVenta)} // Agregamos la funcionalidad de eliminar
+                  >
+                    <i className="fas fa-minus-circle"></i> Eliminar
+                  </button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
