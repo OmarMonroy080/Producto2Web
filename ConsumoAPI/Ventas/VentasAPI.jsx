f54969ed7f8990012a5e8d5b2399f3f8532234fc
Omar <omarpais080@gmail.com>
2023-08-06 17:33:59 -0600

    corrigiendo API ventas

--- ConsumoAPI/Ventas/VentasAPI.jsx
+++ ConsumoAPI/Ventas/VentasAPI.jsx
@@ -1,34 +1,5 @@
 import { showAlerta } from "../Funciones/funciones";
 
-export async function ListarVentas() {
-  const requestOptions = {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
-
-  try {
-    const response = await fetch("http://www.muebleriatroncoso.somee.com/api/Ventas/ListarVentas", requestOptions);
-    const data = await response.json();
-    const mensaje = data.mensaje;
-
-    // Si el mensaje es "ok", significa que la solicitud fue exitosa
-    if (mensaje === "ok") {
-      return data.response; // Devuelve directamente el array de datos (data.response)
-    } else {
-      // Si el mensaje no es "ok", algo salió mal, lanzamos una excepción con el mensaje del servidor
-      throw new Error(mensaje);
-    }
-  } catch (error) {
-    // Capturamos cualquier error en la solicitud o en el procesamiento JSON
-    showAlerta("Error en la solicitud", "error");
-    console.log(error);
-    return []; // Devuelve un array vacío en caso de error para que el componente no falle
-    throw error; // Re-lanzamos el error para que el componente que llama a esta función pueda manejarlo
-  }
-}
-
 export async function ActualizarVenta(parametros, url) {
   const requestOptions = {
     method: "PUT",
@@ -73,28 +44,55 @@ export async function EliminarVenta(idVenta) {
     throw error; // Re-lanzamos el error para que el componente que llama a esta función pueda manejarlo
   }
 }
-const guardarVenta = async (idMueble, idUsuario) => {
-  const parametros = {
-    idMueble: idMueble,
-    idUsuario: idUsuario,
+
+
+export async function ListarVentas() {
+  const requestOptions = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
   };
 
   try {
-    const response = await fetch("http://www.muebleriatroncoso.somee.com/api/Ventas/Guardar", {
-      method: "POST",
+    const response = await fetch("http://www.muebleriatroncoso.somee.com/api/Ventas/ListarVentas", requestOptions);
+    const data = await response.json();
+    const mensaje = data.mensaje;
+
+    // Si el mensaje es "ok", significa que la solicitud fue exitosa
+    if (mensaje === "ok") {
+      return data.response; // Devuelve directamente el array de datos (data.response)
+    } else {
+      // Si el mensaje no es "ok", algo salió mal, lanzamos una excepción con el mensaje del servidor
+      throw new Error(mensaje);
+    }
+  } catch (error) {
+    // Capturamos cualquier error en la solicitud o en el procesamiento JSON
+    showAlerta("Error en la solicitud", "error");
+    console.log(error);
+    return []; // Devuelve un array vacío en caso de error para que el componente no falle
+  }
+}
+
+
+export async function ActualizarGuardarV(metodo, parametros, url) {
+    const requestOptions = {
+      method: metodo,
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(parametros),
-    });
-
-    const data = await response.json();
-    const mensaje = data.mensaje;
-    if (mensaje === "ok") {
-      showAlerta("Venta guardada exitosamente", "success");
+      body: JSON.stringify(parametros),  
+    };
+  
+    try {
+      const response = await fetch(url, requestOptions);
+      const data = await response.json();
+      const mensaje = data.mensaje;
+      if ( mensaje =="ok") {
+        showAlerta(mensaje, "success");
+      }
+    } catch (error) {
+      showAlerta("Error en la solicitud", "error");
+      console.log(error);
     }
-  } catch (error) {
-    showAlerta("Error al guardar la venta", "error");
-    console.log(error);
-  }
-};
+  };
