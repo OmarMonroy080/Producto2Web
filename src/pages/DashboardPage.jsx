7a61ce2f23319f4096a3b578aaadb9b8ade2a167
Omar <omarpais080@gmail.com>
2023-08-06 10:01:25 -0600

    n

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,5 +1,17 @@
-import React from 'react';
-import { Bar } from 'react-chartjs-2';
+import { Bar, Line } from 'react-chartjs-2';
+import { ListarVentas } from '../../ConsumoAPI/Ventas/VentasAPI';
+import { useState, useEffect, useRef } from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  Filler,
+} from 'chart.js';
 
 ChartJS.register(
   CategoryScale,
